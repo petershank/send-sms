217a5df28cc9afa10be5c1193a5dd803aca07b30
Peter Shank <peter@shank.com>
2021-04-12 18:25:55 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require("twilio")(accountSid, authToken);
+
+client.messages
+  .create({
+    body: "This is more secure.",
+    from: "+14259993410",
+    to: "+14259996981",
+  })
+  .then((message) => console.log(message.sid));
